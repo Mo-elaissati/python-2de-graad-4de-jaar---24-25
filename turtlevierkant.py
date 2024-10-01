ad126bc7ba85a51b92e040d1a4a0d2b57cfe0b8e
Mo-elaissati <mohamed.elaissati@kamerksem.be>
2024-10-01 12:52:25 +0200

    Add files via upload

---
+++ turtlevierkant.py
@@ -0,0 +1,20 @@
+# in de wereld van python is een turtle  een klein zwart pijltje dat langzaam over het scherm beweegt.
+# De turtle module is een manier om vectoraafbeeldingen te programmeren, wat eigenlijk gewoon betekent dat je tekent met simple lijnen, stippen en bogen.
+
+# turtle module importeren
+import turtle
+# om de schilderdoek te maken om op tekenen. om dit te doen, roepen we de functie Pen uit de turtle aan, die automatisch een doek maakt. 
+t = turtle.Pen()
+
+# nu zie je een wit veld (het doek) met een pijl in het midden. de pijl in het midden van het vendster is de turtle, die inderdaad niet erg op een schildpad ijkt.
+
+
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+
